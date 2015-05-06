831ac4fb475a1a1bc8a16b2e6fe91737fcfb7bc2
JF Bastien <jfb@google.com>
2015-05-05 22:53:20 -0500

    Update source example, it was missing types, and should use template variables.

--- source/N4509.cc
+++ source/N4509.cc
@@ -1,36 +1,64 @@
 #include <atomic>
 #include <iostream>
 
-// It is implementation-defined what this returns, as long as:
-// if (atomic<T>::is_lock_free())
-//   assert(is_always_lock_free<T>::value);
-template<typename T> struct is_always_lock_free { enum { value = false }; };
+namespace std {
 
-// Implementations must match the C ATOMIC_*_LOCK_FREE macro values.
-template<> struct is_always_lock_free<bool> { enum { value = 2 == ATOMIC_BOOL_LOCK_FREE }; };
-template<> struct is_always_lock_free<char> { enum { value = 2 == ATOMIC_CHAR_LOCK_FREE }; };
-template<> struct is_always_lock_free<char16_t> { enum { value = 2 == ATOMIC_CHAR16_T_LOCK_FREE }; };
-template<> struct is_always_lock_free<char32_t> { enum { value = 2 == ATOMIC_CHAR32_T_LOCK_FREE }; };
-template<> struct is_always_lock_free<wchar_t> { enum { value = 2 == ATOMIC_WCHAR_T_LOCK_FREE }; };
-template<> struct is_always_lock_free<short> { enum { value = 2 == ATOMIC_SHORT_LOCK_FREE }; };
-template<> struct is_always_lock_free<int> { enum { value = 2 == ATOMIC_INT_LOCK_FREE }; };
-template<> struct is_always_lock_free<long> { enum { value = 2 == ATOMIC_LONG_LOCK_FREE }; };
-template<> struct is_always_lock_free<long long> { enum { value = 2 == ATOMIC_LLONG_LOCK_FREE }; };
-template<typename T> struct is_always_lock_free<T*> { enum { value = 2 == ATOMIC_POINTER_LOCK_FREE }; };
+  namespace detail {
+    // It is implementation-defined what this returns, as long as:
+    // if (atomic<T>::is_lock_free())
+    //   assert(is_always_lock_free<T>::value);
+    template<typename T> static constexpr bool is_always_lock_free = false;
+
+    // Implementations must match the C ATOMIC_*_LOCK_FREE macro values.
+    template<> static constexpr bool is_always_lock_free<bool> = 2 == ATOMIC_BOOL_LOCK_FREE;
+    template<> static constexpr bool is_always_lock_free<char> = 2 == ATOMIC_CHAR_LOCK_FREE;
+    template<> static constexpr bool is_always_lock_free<signed char> = 2 == ATOMIC_CHAR_LOCK_FREE;
+    template<> static constexpr bool is_always_lock_free<unsigned char> = 2 == ATOMIC_CHAR_LOCK_FREE;
+    template<> static constexpr bool is_always_lock_free<char16_t> = 2 == ATOMIC_CHAR16_T_LOCK_FREE;
+    template<> static constexpr bool is_always_lock_free<char32_t> = 2 == ATOMIC_CHAR32_T_LOCK_FREE;
+    template<> static constexpr bool is_always_lock_free<wchar_t> = 2 == ATOMIC_WCHAR_T_LOCK_FREE;
+    template<> static constexpr bool is_always_lock_free<short> = 2 == ATOMIC_SHORT_LOCK_FREE;
+    template<> static constexpr bool is_always_lock_free<unsigned short> = 2 == ATOMIC_SHORT_LOCK_FREE;
+    template<> static constexpr bool is_always_lock_free<int> = 2 == ATOMIC_INT_LOCK_FREE;
+    template<> static constexpr bool is_always_lock_free<unsigned int> = 2 == ATOMIC_INT_LOCK_FREE;
+    template<> static constexpr bool is_always_lock_free<long> = 2 == ATOMIC_LONG_LOCK_FREE;
+    template<> static constexpr bool is_always_lock_free<unsigned long> = 2 == ATOMIC_LONG_LOCK_FREE;
+    template<> static constexpr bool is_always_lock_free<long long> = 2 == ATOMIC_LLONG_LOCK_FREE;
+    template<> static constexpr bool is_always_lock_free<unsigned long long> = 2 == ATOMIC_LLONG_LOCK_FREE;
+    template<typename T> static constexpr bool is_always_lock_free<T*> = 2 == ATOMIC_POINTER_LOCK_FREE;
+    template<> static constexpr bool is_always_lock_free<std::nullptr_t> = 2 == ATOMIC_POINTER_LOCK_FREE;
+    // The macros do not support float, double, long double, but C++ does support atomics of these types.
+  }
+
+  template<typename T>
+  class atomic_n4509 {
+    static constexpr bool is_always_lock_free = detail::is_always_lock_free<T>;
+  };
+
+}
 
 int main() {
 
+  template<typename T> using atomic = std::atomic_n4509<T>;
+
   std::cout <<
-    "bool\t" << is_always_lock_free<bool>::value << '\n' <<
-    "char\t" << is_always_lock_free<char>::value << '\n' <<
-    "char16_t\t" << is_always_lock_free<char16_t>::value << '\n' <<
-    "char32_t\t" << is_always_lock_free<char32_t>::value << '\n' <<
-    "wchar_t\t" << is_always_lock_free<wchar_t>::value << '\n' <<
-    "short\t" << is_always_lock_free<short>::value << '\n' <<
-    "int\t" << is_always_lock_free<int>::value << '\n' <<
-    "long\t" << is_always_lock_free<long>::value << '\n' <<
-    "long long\t" << is_always_lock_free<long long>::value << '\n' <<
-    "void*\t" << is_always_lock_free<void*>::value << '\n';
+    "bool\t" << atomic<bool>::is_always_lock_free << '\n' <<
+    "char\t" << atomic<char>::is_always_lock_free << '\n' <<
+    "signed char\t" << atomic<signed char>::is_always_lock_free << '\n' <<
+    "unsigned char\t" << atomic<unsigned char>::is_always_lock_free << '\n' <<
+    "char16_t\t" << atomic<char16_t>::is_always_lock_free << '\n' <<
+    "char32_t\t" << atomic<char32_t>::is_always_lock_free << '\n' <<
+    "wchar_t\t" << atomic<wchar_t>::is_always_lock_free << '\n' <<
+    "short\t" << atomic<short>::is_always_lock_free << '\n' <<
+    "unsigned short\t" << atomic<unsigned short>::is_always_lock_free << '\n' <<
+    "int\t" << atomic<int>::is_always_lock_free << '\n' <<
+    "unsigned int\t" << atomic<unsigned int>::is_always_lock_free << '\n' <<
+    "long\t" << atomic<long>::is_always_lock_free << '\n' <<
+    "unsigned long\t" << atomic<unsigned long>::is_always_lock_free << '\n' <<
+    "long long\t" << atomic<long long>::is_always_lock_free << '\n' <<
+    "unsigned long long\t" << atomic<unsigned long long>::is_always_lock_free << '\n' <<
+    "void*\t" << atomic<void*>::is_always_lock_free << '\n' <<
+    "std::nullptr_t\t" << atomic<std::nullptr_t>::is_always_lock_free << '\n';
 
   return 0;
 }
